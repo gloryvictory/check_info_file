407b7082bdddf2d0e787dc27a3805066c435a4fc
Zamaraev Viacheslav <zamaraev@gmail.com>
2020-07-13 21:03:14 +0500

    1-st commit

---
+++ src/01_check_info_files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#   Author          :   Viacheslav Zamaraev
+#   email           :   zamaraev@gmail.com
+#   Script Name     : 01_check_info_files.py
+#   Created         : 13th July 2020
+#   Last Modified	: 13th July 2020
+#   Version		    : 1.0
+#   PIP             : pip install pewee, psycopg2
+#   RESULT          : csv file with columns: FILENAME;...LASTACCESS
+# Modifications	: 1.1 -
+#               : 1.2 -
+#
+# Description   : get count lines in each csv fle in folder
+
+import os
