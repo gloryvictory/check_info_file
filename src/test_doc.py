48307370a2b7f0702ac89acacf7bc2bd16484507
Zamaraev Viacheslav <zamaraev@gmail.com>
2020-07-21 00:12:50 +0500

    test read doc file

--- src/test_doc.py
+++ src/test_doc.py
@@ -10,6 +10,32 @@
 # txt = transform_doc.transform_doc_stream(content)
 # print(txt)
 
-import textract
-text = textract.process("E:\\Temp\\geodex_test\\1\\INFO.doc")
-print(text)
+# import textract
+# text = textract.process("E:\\Temp\\geodex_test\\1\\INFO.doc")
+# print(text)
+import codecs
+import olefile
+YOUR_DOC = "/Users/glory/projects/geodex_test/1/INFO.doc"
+assert olefile.isOleFile(YOUR_DOC)
+ole = olefile.OleFileIO(YOUR_DOC)
+print(ole.listdir())
+if ole.exists('worddocument'):
+    print("This is a Word document.")
+    t = ole.get_type('WordDocument')
+    print(t)
+    data = ole.openstream('WordDocument').read()
+    data.decode()
+    print(data)
+    #data.decode('cp1251','ignore')
+    #.encode('utf8')
+    print(str(data))
+    # data2 = str(data, 'cp1251')
+    data2=codecs.decode(data, encoding='cp1251', errors='ignore')
+    print(data2)
+    # meta = ole.get_metadata()
+    # print('Author:', meta.author)
+    # print('Title:', meta.title)
+    # print('Creation date:', meta.create_time)
+    # # print all metadata:
+    # meta.dump()
+
